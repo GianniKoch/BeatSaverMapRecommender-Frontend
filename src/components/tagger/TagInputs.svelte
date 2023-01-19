3b9fbd7b754dd360237d514f418db25ea5cbb809
GianniKoch <johannes.koch@student.kdg.be>
2023-01-19 02:08:45 +0100

    Add tag value posting.

--- src/components/tagger/TagInputs.svelte
+++ src/components/tagger/TagInputs.svelte
@@ -1,17 +1,34 @@
 <script lang="ts">
     import TagInput from "./TagInput.svelte";
+    import {setSectionTags} from "../../services/RecommendationBackendService";
+    import {to_number} from "svelte/internal";
 
     export let mapId: string;
     export let time: number;
+    export let loadNextSection = () => {
+    };
+
+    const onSubmit = async (e) => {
+        const formData = new FormData(e.target);
+
+        const tags = [];
+        for (let field of formData) {
+            const [tag, value] = field;
+            tags.push({tag, value: to_number(value)});
+        }
+        await setSectionTags(mapId, time, tags)
+        loadNextSection()
+    }
+
     const tags = ["TrueAcc", "Acc", "TechAcc", "Dance", "Fitness", "Tech", "SpeedTech", "Balanced",
         "MidSpeed", "Speed", "HighSpeed", "Challenge", "Meme"];
 </script>
 
-<div class="grid grid-cols-2 gap-2 px-5">
+<form class="grid grid-cols-2 gap-2 px-5" on:submit|preventDefault={onSubmit}>
     {#each tags as tagLabel}
         <TagInput {tagLabel} class="col"/>
     {/each}
     <div class="col mx-4">
         <button class="btn btn-outline w-full">Set Tag Values</button>
     </div>
-</div>
+</form>
--- src/routes/tagger/+page.svelte
+++ src/routes/tagger/+page.svelte
@@ -1,20 +1,26 @@
 <script lang="ts">
-import BeatSaberViewer from "../../components/tagger/BeatSaberViewer.svelte";
-import ViewerInput from "../../components/tagger/ViewerInput.svelte";
-import TagInput from "../../components/tagger/TagInputs.svelte";
+    import BeatSaberViewer from "../../components/tagger/BeatSaberViewer.svelte";
+    import ViewerInput from "../../components/tagger/ViewerInput.svelte";
+    import TagInput from "../../components/tagger/TagInputs.svelte";
+    import {getNextSection} from "../../services/RecommendationBackendService";
 
-let mapId: string;
-let time: number;
+    let mapId: string;
+    let time: number;
+
+    const loadNextSection = async () => {
+        [mapId, time] = await getNextSection()
+    }
+
+    loadNextSection()
 </script>
 
 <div class="grid grid-cols-3">
-    <!--    Create a half of max width container for an iframe with full height-->
     <div class="col col-span-2">
         <BeatSaberViewer {mapId} {time}/>
     </div>
     <div class="col">
         <ViewerInput bind:mapId bind:time/>
         <hr/>
-        <TagInput {mapId} {time}/>
+        <TagInput {mapId} {time} {loadNextSection} />
     </div>
 </div>
